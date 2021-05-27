7bb76d77307b0720c3cb3b8a8105b9072cf34db0
charmseer <charmseer@gmail.com>
2021-05-27 10:09:18 +0530

    Setup Player movement L to R

--- Dodgeblocks/Assets/PlayerControls.cs
+++ Dodgeblocks/Assets/PlayerControls.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 15f;
+
+    private Rigidbody2D rb;
     void Start()
     {
-        
-    }
+        rb = GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
-    void Update()
+    }
+    void FixedUpdate()
     {
-        
+        float x = Input.GetAxis("Horizontal")* Time.fixedDeltaTime * speed;
+
+        //rb.position is in (x,y) format so you can only add something
+        //else in the same format. Vector2.right adds a vector that is (1,0)
+        rb.MovePosition(rb.position + Vector2.right * x);
     }
 }
