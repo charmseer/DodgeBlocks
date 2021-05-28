3cecb8d566beec0c6b1b53db1ab5048b15520f34
charmseer <charmseer@gmail.com>
2021-05-28 18:09:06 +0530

    Organize into folders, Add GameOver Panel and activation

--- Dodgeblocks/Assets/Scripts/PlayerCollision.cs
+++ Dodgeblocks/Assets/Scripts/PlayerCollision.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerControls movement;
+    public GameObject gameOverPanel;
     void OnCollisionEnter2D()
     {
         Debug.Log("We've been hit!");
+
+        // Disables the Playermovement script
         movement.enabled = false;
+
+        //Shows the GameOver Screen
+
+        gameOverPanel.SetActive(true);
+
+
         FindObjectOfType<GameManager>().EndGame();
     }
 
